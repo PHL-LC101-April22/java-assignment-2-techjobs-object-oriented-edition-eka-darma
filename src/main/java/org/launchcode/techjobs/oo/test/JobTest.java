c939125c64475dba81ea403a9e35503f5a264441
Eka Darmawan <eka_darma@hotmail.com>
2022-08-18 21:35:29 -0400

    Test Four Passed

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,50 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    @Test
+    public void testSettingJobId() {
+        Job testJob1 = new Job();
+        Job testJob2 = new Job();
+        assertNotEquals(testJob1.getId(), testJob2.getId() + 1);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertEquals(testJob3.getName(), "Product tester");
+        assertEquals(testJob3.getEmployer().getValue(),"ACME");
+        assertEquals(testJob3.getLocation().getValue(), "Desert");
+        assertEquals(testJob3.getPositionType().getValue(), "Quality control");
+        assertEquals(testJob3.getCoreCompetency().getValue(), "Persistence");
+
+        assertTrue(testJob3.getName() != null);
+        assertTrue(testJob3.getEmployer() != null);
+        assertTrue(testJob3.getLocation() != null);
+        assertTrue(testJob3.getPositionType() != null);
+        assertTrue(testJob3.getCoreCompetency() != null);
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        //Job testJob4 = new Job();
+        //Job testJob5 = new Job();
+        Job testJob4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job testJob5 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertFalse(testJob4 == testJob5);
+    }
+
+
 
 }
